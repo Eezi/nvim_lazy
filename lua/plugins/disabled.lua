125f77d7244277dfee26b103ce2e48b0a410cefa
Eezi <tomi.a.salo@outlook.com>
2023-12-29 09:49:25 +0200

    disabled unused plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,10 @@
 return {
+  -- TODO: Remove all unused plugins
   { "echasnovski/mini.animate", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "iamcco/markdown-preview.nvim", enabled = false },
+  { "mfussenegger/nvim-dap-python", enabled = false },
+  { "linux-cultist/venv-selector.nvim", enabled = false },
+  { "nvim-neotest/neotest", enabled = false },
 }
